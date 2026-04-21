70510c72056c46fed34010f943c5dd84b30ac51f
ranashish0401 <ashishgautamrana731@gmail.com>
2026-04-21 23:47:42 +0530

    Add files via upload

---
+++ Student_Management_System/Student.java
@@ -0,0 +1,18 @@
+class Student {
+    int id;
+    String name;
+    int age;
+    String course;
+
+    Student(int id, String name, int age, String course) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name +
+                ", Age: " + age + ", Course: " + course);
+    }
+}
